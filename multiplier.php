482f45ff88de2855be20083f0d5af902453dc241
Rick de Jong <65729924+rdejong2006@users.noreply.github.com>
2024-03-18 11:03:28 +0100

    Add files via upload

---
+++ multiplier.php
@@ -0,0 +1,11 @@
+<?php
+
+echo"ik ga twee getallen met elkaar vermenigvuldigen" . PHP_EOL;
+echo "eerste getal?" . PHP_EOL;
+
+$a = readline();
+echo"tweede getal?" . PHP_EOL;
+$b = readline();
+echo"Uw reseltaat: " . $a * $b;
+
+?> 
